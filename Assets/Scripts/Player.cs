c3b159e3f3b83538612204d83908487abc621cfd
DSeaNguyen <nguyendinhhoangquan1825@gmail.com>
2026-03-10 08:42:21 +0700

    Sửa di chuyển

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,33 +2,51 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 5f;     
-    public float rotateSpeed = 120f; 
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 200f;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+    private float rotateInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        float randomAngle = Random.Range(0f, 360f);
+        transform.rotation = Quaternion.Euler(0, 0, randomAngle);
+    }
 
     void Update()
     {
-        float move = 0f;
+        moveInput = 0f;
+
         if (Input.GetKey(KeyCode.W))
         {
-            move = 1f;
+            moveInput = 1f;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            move = -1f;
+            moveInput = -1f;
         }
 
-        transform.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime);
+        rotateInput = 0f;
 
-        float rotate = 0f;
         if (Input.GetKey(KeyCode.A))
         {
-            rotate = -1f;
+            rotateInput = 1f;
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            rotate = 1f;
+            rotateInput = -1f;
         }
+    }
 
-        transform.Rotate(Vector3.up * rotate * rotateSpeed * Time.deltaTime);
+    void FixedUpdate()
+    {
+        rb.MoveRotation(rb.rotation + rotateInput * rotateSpeed * Time.fixedDeltaTime);
+
+        Vector2 movement = transform.right * moveInput * moveSpeed;
+        rb.linearVelocity = movement;
     }
 }
