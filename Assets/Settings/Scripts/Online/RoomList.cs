664288f2e79813d1b64dd7783f7c2233227d7a4f
Ryder <doanryder2005@gmail.com>
2026-06-24 15:51:25 +0700

    Sửa lỗi tạo 2 phòng trở lên cùng tên

--- Assets/Settings/Scripts/Online/RoomList.cs
+++ Assets/Settings/Scripts/Online/RoomList.cs
@@ -6,17 +6,30 @@ using UnityEngine.UI;
 
 public class RoomList : MonoBehaviourPunCallbacks
 {
-    [SerializeField] CreateAndJoin createAndJoin;
     [SerializeField] GameObject roomPrefab;
+    GameObject[] allRooms = new GameObject[0];
 
     public override void OnRoomListUpdate(List<RoomInfo> roomList)
     {
+        for (int i = 0; i < allRooms.Length; i++)
+        {
+            if (allRooms[i] != null)
+                Destroy(allRooms[i]);
+        }
+
+        allRooms = new GameObject[roomList.Count];
+
         for (int i = 0; i < roomList.Count; i++)
         {
-            GameObject room = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, transform);
+            if (roomList[i].IsOpen && roomList[i].IsVisible && roomList[i].PlayerCount >= 1)
+            {
+                GameObject room = Instantiate(roomPrefab, Vector3.zero, Quaternion.identity, transform);
 
-            RoomElement roomElement = room.GetComponent<RoomElement>();
-            roomElement.roomText.text = roomList[i].Name;
+                RoomElement roomElement = room.GetComponent<RoomElement>();
+                roomElement.roomText.text = roomList[i].Name;
+
+                allRooms[i] = room;
+            }
         }
     }
 }
